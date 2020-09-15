cdae2a5bd68222c59a8e78532b97489766a01bcb
Balvine <balvy075@gmail.com>
2020-09-15 11:55:33 +0300

    added new folders

--- instance/config.py
+++ instance/config.py
@@ -1,5 +1,5 @@
 MOVIE_API_KEY = '2a7aa1337248c047b356927474f962b0'
-SECRET_KEY = '<Flask WTF Secret Key>'
+SECRET_KEY = 'barl'
 import os
 
 class Config:
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY='2a7aa1337248c047b356927474f962b0'
+export SECRET_KEY='barl'
+
+python3.6 manage.py server
